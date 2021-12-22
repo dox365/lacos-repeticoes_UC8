27f769c2901caba0ac1ecf0dbcec6d4fe476a896
dox365 <dox365@gmail.com>
2021-12-22 19:09:14 -0300

    atividade do encontro remoto 2

---
+++ senai-lacos-repeticoes.js
@@ -0,0 +1,34 @@
+let totalAlunos = 10;
+
+ for (let contadorAluno = 0; contadorAluno <= totalAlunos; contadorAluno++) {
+console.log(contadorAluno);
+    
+     if (contadorAluno == 0) {
+        console.log("O número atual é zero")
+     }else if (contadorAluno % 2 == 0) {
+         console.log("O número " + contadorAluno + " é par")
+     } else {
+         console.log(`O número ${contadorAluno} é ímpar`)
+     }
+ }
+
+ let contadorAluno = 0;
+
+ while (contadorAluno <= totalAlunos) {
+     //console.log(contadorAluno);
+    
+     if (contadorAluno == 0) {
+         console.log("O número atual é zero")
+     } else if (contadorAluno % 2 != 0) {
+         console.log("O número " + contadorAluno + " é ímpar")
+     } else {
+         console.log(`O número ${contadorAluno} é par`)
+     }
+     contadorAluno++
+ }
+
+let listaAlunos = ["Cleusa", "Danilo", "Patricia", "Antonia"]
+
+for (let aluno of listaAlunos) {
+    console.log(`Este aluno se chama ${aluno}`)
+}
